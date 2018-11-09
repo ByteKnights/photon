95646e761ad613a91e79caabdccc89f24c7019fc
Nathan Robinson <nrobinson2000@me.com>
2018-11-08 20:09:42 -0500

    at 20:09 of 2018-11-08

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,11 +1,19 @@
 #include "Particle.h"
 
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
 void setup() // Put setup code here to run once
 {
+
+        pinMode(D7, OUTPUT);
 
 }
 
 void loop() // Put code here to loop forever
 {
+
+        digitalWrite(D7, !digitalRead(D7));
+        delay(500);
 
 }
