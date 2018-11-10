0e34e66b47c14a38ff4a02db533379842507a535
Nathan Robinson <nrobinson2000@me.com>
2018-11-10 12:55:08 -0500

    Send any input to discord at 12:55 of 2018-11-10

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -3,17 +3,44 @@
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
 
-void setup() // Put setup code here to run once
-{
-
+void setup() { // Put setup code here to run once
         pinMode(D7, OUTPUT);
-
+        Serial.begin(115200);
+        Particle.connect();
 }
 
-void loop() // Put code here to loop forever
-{
+const size_t READ_BUF_SIZE = 622;
+char readBuf[READ_BUF_SIZE];
+size_t readBufOffset = 0;
 
-        digitalWrite(D7, !digitalRead(D7));
-        delay(500);
+void processBuffer() {
+  if (Particle.connected()) {
+    Particle.publish("discord", readBuf, PRIVATE);
+  }
+}
 
+void serialHandler() {
+        while(Serial.available()) {
+                if (readBufOffset < READ_BUF_SIZE) {
+                        char c = Serial.read();
+                        if (c != '\n') {
+                                // Add character to buffer
+                                readBuf[readBufOffset++] = c;
+                        } else {
+                                // End of line character found, process line
+                                readBuf[readBufOffset] = 0;
+                                processBuffer();
+                                readBufOffset = 0;
+                        }
+                } else {
+                        Serial.println("readBuf overflow, emptying buffer");
+                        readBufOffset = 0;
+                }
+        }
+}
+
+void loop() { // Put code here to loop forever
+        if (Serial.available()) {
+                serialHandler();
+        }
 }
